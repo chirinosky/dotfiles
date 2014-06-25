06ce87172c8043e90888c44f328fc91e08f9bcef
Ramces Chirino <ramces@chirinosky.com>
2014-06-25 19:19:22 -0400

    Initial install script

---
+++ README.md
@@ -0,0 +1,5 @@
+## tl;dr ##
+```bash
+sh -c "`curl -fsSL
+https://raw.github.com/chirinosky/dotfiles/master/install.sh`"
+```
---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ ! -d "$HOME/.dotfiles" ]; then
+    echo "Starting new .dotfiles install..."
+    git clone https://github.com/chirinosky/dotfiles.git "$HOME/.dotfiles"
+    cd "$HOME/.dotfiles"
+    python install.py
+else
+    echo "Install aborted because .dotfiles folder is present"
+fi
