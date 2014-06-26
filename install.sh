11a5387ae836103062471e52a22575128db25d54
Ramces Chirino <ramces@chirinosky.com>
2014-06-26 16:47:55 -0400

    updated initial install & removed python install script

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 if [ ! -d "$HOME/.dotfiles" ]; then
     echo "Starting new .dotfiles install..."
-    $(git clone https://github.com/chirinosky/dotfiles.git $HOME/.dotfiles)
-    $(cd $HOME/.dotfiles/scripts)
-    $(./bootstrap.sh)
+    git clone https://github.com/chirinosky/dotfiles.git $HOME/.dotfiles
+    cd $HOME/.dotfiles/scripts
+    sh bootstrap.sh
 else
     echo "Install aborted because a .dotfiles folder is present"
 fi
