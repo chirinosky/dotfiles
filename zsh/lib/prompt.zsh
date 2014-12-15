4840fdb3687006192d6f900d0be0af40dd9f0119
Ramces Chirino <ramces@chirinosky.com>
2014-12-14 20:37:04 -0500

    moved prompt configs out of zshrc

---
+++ zsh/lib/prompt.zsh
@@ -0,0 +1,20 @@
+# ==================================================
+# PROMPT
+# ==================================================
+setopt prompt_subst                                   # Evaluate prompt whenever displayed
+autoload -Uz colors && colors                         # Colorize output
+
+for COLOR in BLACK RED GREEN YELLOW BLUE MAGENTA CYAN WHITE; do
+    FG_$COLOR='%{fg_no_bold[$COLOR]%}'
+done
+
+ZSH_THEME_GIT_PROMPT_PREFIX="$FG_GREY($FG_RED"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[grey]%}) %{$fg[yellow]%}⚡%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[grey]%})"
+ZSH_THEME_GIT_PROMPT_BEHIND_REMOTE="%{$fg_bold[magenta]%}↓%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_AHEAD_REMOTE="%{$fg_bold[magenta]%}↑%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIVERGED_REMOTE="%{$fg_bold[magenta]%}↕%{$reset_color%}"
+
+PROMPT='%{$fg_bold[grey]%}[%{$reset_color%}%{$fg_bold[green]%}%n@%m%{$reset_color%}%{$fg_bold[grey]%}]%{$reset_color%} %{$fg_bold[blue]%}%10c%{$reset_color%} $(git_prompt_info) $(git_remote_status)
+%{$fg_bold[cyan]%}❯%{$reset_color%} '
