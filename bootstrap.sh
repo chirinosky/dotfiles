fc42581d639db4cd11d939effde4e8fa6974594b
Ramces Chirino <ramces@chirinosky.com>
2014-12-20 17:21:09 -0500

    cleaned up code

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,66 +1,91 @@
 #!/bin/bash
 
-install_git() {
-    if [ -z "$(command -v git)" ]; then
-        echo -n "Installation"
+readonly OLDDOTFILES="$HOME/olddotfiles"
+
+function backup_dotfile() {
+    local dotfiles
+    for file in $(ls -a $HOME |grep $1); do
+        dotfiles+=($file)
+    done
+    if [[ ${#dotfiles[@]} -gt 0 ]]; then
+        local DOTFOLDER="$OLDDOTFILES/$1"
+        echo "Backing up existing configs..."
+        mkdir -p "$DOTFOLDER"
+        for file in ${dotfiles[@]}; do
+            mv "$HOME/$file" "$DOTFOLDER/$file"
+        done
+    fi
+}
+
+function configure_git() {
+    echo "Configuring git..."
+    backup_dotfile "git"
+    cp "$HOME/.dotfiles/git/gitconfig" "$HOME/.gitconfig"
+    cp "$HOME/.dotfiles/git/gitignore" "$HOME/.gitignore"
+}
+
+function configure_gnome_terminal() {
+    echo "Configuring gnome-terminal..."
+    if [[ -z "$(command -v gnome-terminal)" ]]; then
+        echo "Not found, skipping."
+        return 0
+    fi
+    gconftool-2 --load $HOME/.dotfiles/terminals/gnome-terminal-conf.xml
+}
+
+function configure_vim() {
+    echo "Configuring vim..."
+    backup_dotfile "vim"
+    cp "$HOME/.dotfiles/vim/vimrc" "$HOME/.vimrc"
+}
+
+function configure_zsh() {
+    echo "Configuring zsh..."
+    backup_dotfile "zsh"
+    cp "$HOME/.dotfiles/zsh/zshrc" "$HOME/.zshrc"
+    sudo chsh -s "$(command -v zsh)"
+}
+
+function install_git() {
+    if [[ -z "$(command -v git)" ]]; then
+        echo -n "Installing git..."
         sudo apt-get -y install git >& /dev/null
+        echo "done"
+        configure_git
     else
         echo "git found, skipping install"
     fi
 }
 
-configure_git() {
-    echo -n "Configuring git..."
-    ln -s "$HOME/.dotfiles/git/gitconfig" "$HOME/.gitconfig"
-    ln -s "$HOME/.dotfiles/git/gitignore" "$HOME/.gitignore"
-    echo "done."
-}
-
-symlink_vim() {
-    OLDDOTFILES="$HOME/olddotfiles"
-
-    # Clean up old vim files
-    for file in $(ls -a $HOME |grep vim); do
-        vim_files+=($file)
-    done
-    if [ ${#vim_files[@]} -gt 0 ]; then
-        echo -n "Backing up existing vim runtime configs..."
-        mkdir -p "$OLDDOTFILES/vim"
-        for file in ${vim_files[@]}; do
-            mv "$HOME/$file" "$OLDDOTFILES/vim/$file"
-        done
-    fi
-
-    # Clean up old vi files
-    vi_file=$(ls -a $HOME |grep .exrc)
-
-    if [ -n "$vi_file" ]; then
-        mkdir -p $OLDDOTFILES/vi
-        mv $HOME/$vi_file $OLDDOTFILES/vi/$vi_file
-    fi
-    echo "done."
-
-    echo -n "Linking to new vimrc..."
-    ln -s "$HOME/.dotfiles/vim/vimrc" "$HOME/.vimrc"
-    echo "done."
-}
-
-upgrade_vim() {
-    if [ -z "$(command -v vim)" ]; then
-        echo -n "Installing required vim packages..."
-        sudo apt-get -y install vim-gui-common vim-runtime >& /dev/null
+function install_vim() {
+    if [[ -z "$(command -v vim)" ]]; then
+        echo -n "Installing vim..."
+        sudo apt-get install -y vim-gui-common vim-runtime >& /dev/null
         echo "done."
+        configure_vim
+        install_vim_plugins
     else
         echo "vim found, skipping installation..."
     fi
 }
 
-configure_vim_plugins() {
+function install_zsh() {
+    if [[ -z "$(command -v zsh)" ]]; then
+        echo -n "Installing zsh..."
+        sudo apt-get install -y zsh >& /dev/null
+        echo "done"
+        configure_zsh
+    else
+        echo "zsh found...skipping install."
+    fi
+}
+
+function install_vim_plugins() {
     echo -n "Installing vim plugins..."
     git clone https://github.com/gmarik/Vundle.vim.git \
         $HOME/.dotfiles/vim/bundle/Vundle.vim >& /dev/null
     vim -i NONE -c VundleUpdate -c quitall
-    echo "done."
+    echo "done"
     echo -n "Installing Powerline fonts..."
     FONTS="$HOME/.fonts"
     FONTCFG="$HOME/.fonts.conf.d"
@@ -74,33 +99,6 @@ configure_vim_plugins() {
     echo "done."
 }
 
-configure_gnome_terminal() {
-    echo -n "Configuring gnome-terminal..."
-    if [ -z "$(command -v gnome-terminal)" ]; then
-        echo "Not found, skipping."
-        return 0
-    fi
-    gconftool-2 --load $HOME/.dotfiles/terminals/gnome-terminal-conf.xml
-    echo "done."
-}
-
-install_zsh() {
-    if [ -z "$(command -v zsh)" ]; then
-        echo -n "Installing zsh..."
-        sudo apt-get install -y zsh >& /dev/null
-        echo "done."
-    else
-        echo "zsh installation found...skipping install."
-    fi
-}
-
-configure_zsh() {
-    echo -n "Linking to new zshrc..."
-    ln -s "$HOME/.dotfiles/zsh/zshrc" "$HOME/.zshrc"
-    echo "done."
-    sudo chsh -s "$(command -v zsh)"
-}
-
 if [ ! -d "$HOME/.dotfiles" ]; then
     echo -n "Updating system repos..."
     sudo apt-get update >& /dev/null
@@ -111,11 +109,7 @@ else
     echo "Aborted because a .dotfiles folder is present"
     exit
 fi
-configure_git
 configure_gnome_terminal
 install_zsh
-configure_zsh
-upgrade_vim
-symlink_vim
-configure_vim_plugins
+install_vim
 printf "Restart your desktop session to ensure all settings took place."
