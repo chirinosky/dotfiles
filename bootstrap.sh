539043cdf2af116303c0365b97119bee8818c893
root <root@localhost.localdomain>
2019-04-19 23:15:52 -0400

    add sublime install commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,4 +15,6 @@ if [ "$OS" = "linux" ]; then
 	# Install Sublime Text
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 	echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+	apt update
+	apt install -y sublime-text
 fi
