ed45e69ba4a77fbe389fc4278c30c716d7c07d31
root <root@localhost.localdomain>
2019-04-19 23:13:12 -0400

    set sublime stable channel

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,5 +13,6 @@ if [ "$OS" = "linux" ]; then
 	done
 
 	# Install Sublime Text
-	$(wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -)
+	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+	echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
 fi
