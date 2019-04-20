6fda60b3c402e8a911a7702337e2d249b03b1212
root <root@localhost.localdomain>
2019-04-19 22:12:51 -0400

    prepare apt for sublime downloading and add gpg keys

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,11 @@ export OS=`uname -s | sed -e 's/  */-/g;y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijk
 
 if [ "$OS" = "linux" ]; then
 	# Install basic packages
-	for pkg in build-essential python-dev vim git
+	for pkg in build-essential python-dev vim git apt-transport-https
 	do
-		apt install -y $pkg
+		$(apt install -y $pkg)
 	done
+
+	# Install Sublime Text
+	$(wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -)
 fi
