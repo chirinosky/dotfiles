c983e2f4d66fddba30c4c4a88d7ff517d8099ce4
Ramces Chirino <ramces@chirinosky.com>
2019-08-22 22:27:43 -0400

    Refactor git configs

---
+++ git/git.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Install
+apt install -y git
+
+cd git/
+
+# Link/Copy configs
+PREF_PATH=$HOME/.config/git
+test -d $PREF_PATH || mkdir -p $PREF_PATH
+cp gitconfig $PREF_PATH/config
+ln -s $(pwd)/gitmessage $PREF_PATH/message
+ln -s $(pwd)/gitignore $PREF_PATH/ignore
+
+# Non-public settings
+echo "Enter email address to use with git: "
+read gitemail
+sed -i "s/EMAIL_PLACEHOLDER/$gitemail/g" $PREF_PATH/config
+
+echo "Enter full name: "
+read gitfullname
+sed -i "s/NAME_PLACEHOLDER/$gitfullname/g" $PREF_PATH/config
