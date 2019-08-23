9f6a9a3ec093cd9ab9fffdf51ad985ca7f7e1915
Ramces Chirino <ramces@chirinosky.com>
2019-08-22 22:37:41 -0400

    Refactor vim setup

---
+++ vim/vim.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd vim/
+
+# Install packages
+apt install -y vim fonts-powerline
+
+# Preferences
+cp vimrc.template $HOME/.vimrc
+
+# Setup Vundle
+test -d bundle/Vundle.vim || git clone https://github.com/gmarik/Vundle.vim.git bundle/Vundle.vim
+vim +BundleInstall +qall
+
+# Powerline fonts
+wget -qO - https://raw.githubusercontent.com/powerline/fonts/master/install.sh |bash
