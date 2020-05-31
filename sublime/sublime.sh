7300d7467b4a5391a6a2b6684673f5bb10446361
Ramces Chirino <ramces@chirinosky.com>
2020-05-31 15:28:54 -0400

    use sudo

--- sublime/sublime.sh
+++ sublime/sublime.sh
@@ -6,7 +6,7 @@ cd sublime/
 # Install Sublime
 sudo apt install -y apt-transport-https
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
 sudo apt install -y sublime-text
 
