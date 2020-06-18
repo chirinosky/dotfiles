fce005a27ef584900f7dcf15f504d2fce7d60c00
Ramces Chirino <ramces@chirinosky.com>
2020-06-17 20:01:31 -0400

    adds parrot os support

--- bash/bash.sh
+++ bash/bash.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
+if [[ $SHLVL -lt 3 ]]; then
+	if [ -f /etc/os-release ]; then
+		. /etc/os-release
+		OS=$NAME
+	fi
+
+	case $OS in
+		"Parrot GNU/Linux")
+			parrot=true
+			;;
+	esac
+fi
+
 cd bash/
 
 cp "$(pwd)/bashrc.template" "$HOME/.bashrc"
+
+if [ $parrot ]; then
+	echo "export PATH=~/.local/bin:/snap/bin:/usr/sandbox/:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/usr/share/games:/usr/local/sbin:/usr/sbin:/sbin:$PATH" >> "$HOME/.bashrc"
+fi
+
 source "$HOME/.bashrc"
--- setup.sh
+++ setup.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
+set -a
 
-sudo apt update && apt upgrade -y
+if [ -f /etc/os-release ]; then
+	. /etc/os-release
+	OS=$NAME
+fi
 
-cd "$(dirname "$0")"
-scripts/app_installs.sh
-bash/bash.sh
-tmux/tmux.sh
-git/git.sh
-gnome/gnome.sh
-cherrytree/cherrytree.sh
-vim/vim.sh
-sublime/sublime.sh
+case $OS in
+	"Parrot GNU/Linux")
+		parrot=true
+		;;
+esac
+
+if [ $parrot ]; then
+	sudo apt update && apt full-upgrade -y
+	cd "$(dirname "$0")"
+	scripts/app_installs.sh
+	git/git.sh
+	bash/bash.sh
+	tmux/tmux.sh
+	cherrytree/cherrytree.sh
+	vim/vim.sh
+	sublime/sublime.sh
+elif [ $kali ]; then
+	gnome/gnome.sh
+fi
+
+# If VM using vmware tools
+# /etc/fstab
+# .host:/		/mnt/hgfs/		fuse.vmhgfs-fuse		defaults,allow_other,uid=1000		0		0
