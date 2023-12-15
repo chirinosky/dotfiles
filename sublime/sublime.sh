6c7114c1beb22eded4421c51395b8fa65b951242
Ramces Chirino <2205694+chirinosky@users.noreply.github.com>
2023-12-14 19:46:25 -0500

    Removed the Python Black package

--- sublime/sublime.sh
+++ sublime/sublime.sh
@@ -6,7 +6,6 @@ cd sublime/
 # Install Sublime
 sudo apt install -y apt-transport-https
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-#wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
 sudo apt install -y sublime-text
@@ -22,10 +21,6 @@ PKG_SETTINGS_PATH="$HOME/.config/sublime-text-3/Packages/User"
 test -d $PKG_SETTINGS_PATH || mkdir -p $PKG_SETTINGS_PATH
 cp "Package Control.sublime-settings" $PKG_SETTINGS_PATH
 
-# # Needed for sublack to run
-# sudo apt install -y python3-pip
-#pip3 install black
-
 # PREFERENCES
 # ** USER
 if [ -f "$PKG_SETTINGS_PATH/Preferences.sublime-settings" ]; then
@@ -34,8 +29,5 @@ fi
 ln -s "$(pwd)/Preferences.sublime-settings" "$PKG_SETTINGS_PATH/Preferences.sublime-settings"
 # **** key bindings
 ln -s "$(pwd)/Default (Linux).sublime-keymap" "$PKG_SETTINGS_PATH/Default (Linux).sublime-keymap"
-# ** PACKAGES
-# **** sublack
-#ln -s "$(pwd)/sublack.sublime-settings" "$PKG_SETTINGS_PATH/sublack.sublime-settings"
 
 subl &
